8b49e6ed221e27a84937ce5acdbf9429b7691021
梦泽 陈 <1398947448@qq.com>
2024-06-14 23:51:05 +0800

    stupid retard GB2312->utf8

--- ASSET/2023级C++大作业题目及要求/C++大作业【下学期】/第X组【组员1、组员2、组员3、组员4】探索性题目1/探索性题目1【图像的矩形NAM分割方法探索】/Diagonal_Priority/Header.h
+++ ASSET/2023级C++大作业题目及要求/C++大作业【下学期】/第X组【组员1、组员2、组员3、组员4】探索性题目1/探索性题目1【图像的矩形NAM分割方法探索】/Diagonal_Priority/Header.h
@@ -1,64 +1,64 @@
-#include<opencv.hpp>
+#include <cmath>
+#include <iostream>
+#include <opencv.hpp>
 #include <opencv2\imgproc\types_c.h>
-#include<iostream>
-#include<vector>
-#include<cmath>
+#include <vector>
 #include <windows.h>
 using namespace cv;
 using namespace std;
 
-//—’…´±Ì
-class Color
-{
-	Point lt;
-	Point rb;
-	Scalar grey;
-public:
-	Color() { grey[0] = grey[1] = grey[2] = grey[3] = -1; }
-	void setLt(Point lt) { this->lt = lt; }
-	void setRb(Point rb) { this->rb = rb; }
-	Point getLt() { return lt; }
-	Point getRb() { return rb; }
-	void setGrey(Scalar grey) { this->grey = grey; }
-	Scalar getGrey() { return grey; }
+// È¢úËâ≤Ë°®
+class Color {
+    Point lt;
+    Point rb;
+    Scalar grey;
+
+  public:
+    Color() { grey[0] = grey[1] = grey[2] = grey[3] = -1; }
+    void setLt(Point lt) { this->lt = lt; }
+    void setRb(Point rb) { this->rb = rb; }
+    Point getLt() { return lt; }
+    Point getRb() { return rb; }
+    void setGrey(Scalar grey) { this->grey = grey; }
+    Scalar getGrey() { return grey; }
 };
-//º∆ ±
+// ËÆ°Êó∂
 class MyTimer {
-private:
-	int _freq;
-	LARGE_INTEGER _begin;
-	LARGE_INTEGER _end;
-public:
-	long costTime;            // ª®∑—µƒ ±º‰(æ´»∑µΩŒ¢√Î)
-public:
-	MyTimer() {
-		LARGE_INTEGER tmp;
-		QueryPerformanceFrequency(&tmp);
-		_freq = tmp.QuadPart;
-		costTime = 0;
-	}
-	void Start() {            // ø™ ºº∆ ±
-		QueryPerformanceCounter(&_begin);
-	}
-	void End() {                // Ω· ¯º∆ ±
-		QueryPerformanceCounter(&_end);
-		costTime = (long)((_end.QuadPart - _begin.QuadPart) * 1000 / _freq);
-	}
-	void Reset() {            // º∆ ±«Â0
-		costTime = 0;
-	}
+  private:
+    int _freq;
+    LARGE_INTEGER _begin;
+    LARGE_INTEGER _end;
+
+  public:
+    long costTime; // Ëä±Ë¥πÁöÑÊó∂Èó¥(Á≤æÁ°ÆÂà∞ÂæÆÁßí)
+  public:
+    MyTimer() {
+        LARGE_INTEGER tmp;
+        QueryPerformanceFrequency(&tmp);
+        _freq = tmp.QuadPart;
+        costTime = 0;
+    }
+    void Start() { // ÂºÄÂßãËÆ°Êó∂
+        QueryPerformanceCounter(&_begin);
+    }
+    void End() { // ÁªìÊùüËÆ°Êó∂
+        QueryPerformanceCounter(&_end);
+        costTime = (long)((_end.QuadPart - _begin.QuadPart) * 1000 / _freq);
+    }
+    void Reset() { // ËÆ°Êó∂Ê∏Ö0
+        costTime = 0;
+    }
 };
 
-
-
-bool isSameBlock(Mat f,Mat mark, Point lt, Point rb,int xgm);
+bool isSameBlock(Mat f, Mat mark, Point lt, Point rb, int xgm);
 void mark(Mat &mark, Point lt, Point rb);
 void EnCode(Mat &R, int height, int width, vector<char> &Q);
 void Decode(Mat &R, int height, int width, const vector<char> &Q);
 Point REHelper(Mat f, Mat mark, int M, int N, Point start, int xgm);
-void RNAMCEncoding(Mat f, Mat &R, Mat &markMatrix, int M, int N, vector<Color> &p, vector<char> &q, int xgm);
-void RDHelper(Mat& R, Color p1);
-void RNAMCDecoding(Mat& R, int M, int N, vector<Color> p, vector<char> q);
+void RNAMCEncoding(Mat f, Mat &R, Mat &markMatrix, int M, int N,
+                   vector<Color> &p, vector<char> &q, int xgm);
+void RDHelper(Mat &R, Color p1);
+void RNAMCDecoding(Mat &R, int M, int N, vector<Color> p, vector<char> q);
 double PSNR(Mat f, Mat R, int M, int N);
 double BPP(vector<Color> p, int M, int N, vector<char> q);
 void segmentDisplay(Mat &display, vector<Color> p);
--- ASSET/2023级C++大作业题目及要求/C++大作业【下学期】/第X组【组员1、组员2、组员3、组员4】探索性题目1/探索性题目1【图像的矩形NAM分割方法探索】/Horizonal_Priority/Header.h
+++ ASSET/2023级C++大作业题目及要求/C++大作业【下学期】/第X组【组员1、组员2、组员3、组员4】探索性题目1/探索性题目1【图像的矩形NAM分割方法探索】/Horizonal_Priority/Header.h
@@ -1,62 +1,63 @@
-#include<opencv.hpp>
-#include<iostream>
-#include<vector>
-#include<cmath>
+#include <cmath>
+#include <iostream>
+#include <opencv.hpp>
+#include <vector>
 #include <windows.h>
 using namespace cv;
 using namespace std;
 
-//ÑÕÉ«±í
-class Color
-{
-	Point lt;
-	Point rb;
-	Scalar grey;
-public:
-	Color() { grey[0] = grey[1] = grey[2] = grey[3] = -1; }
-	void setLt(Point lt) { this->lt = lt; }
-	void setRb(Point rb) { this->rb = rb; }
-	Point getLt() { return lt; }
-	Point getRb() { return rb; }
-	void setGrey(Scalar grey) { this->grey = grey; }
-	Scalar getGrey() { return grey; }
-};
-//¼ÆÊ±
-class MyTimer {
-private:
-	int _freq;
-	LARGE_INTEGER _begin;
-	LARGE_INTEGER _end;
-public:
-	long costTime;            // »¨·ÑµÄÊ±¼ä(¾«È·µ½Î¢Ãë)
-public:
-	MyTimer() {
-		LARGE_INTEGER tmp;
-		QueryPerformanceFrequency(&tmp);
-		_freq = tmp.QuadPart;
-		costTime = 0;
-	}
-	void Start() {            // ¿ªÊ¼¼ÆÊ±
-		QueryPerformanceCounter(&_begin);
-	}
-	void End() {                // ½áÊø¼ÆÊ±
-		QueryPerformanceCounter(&_end);
-		costTime = (long)((_end.QuadPart - _begin.QuadPart) * 1000 / _freq);
-	}
-	void Reset() {            // ¼ÆÊ±Çå0
-		costTime = 0;
-	}
-};
+// é¢œè‰²è¡¨
+class Color {
+    Point lt;
+    Point rb;
+    Scalar grey;
 
+  public:
+    Color() { grey[0] = grey[1] = grey[2] = grey[3] = -1; }
+    void setLt(Point lt) { this->lt = lt; }
+    void setRb(Point rb) { this->rb = rb; }
+    Point getLt() { return lt; }
+    Point getRb() { return rb; }
+    void setGrey(Scalar grey) { this->grey = grey; }
+    Scalar getGrey() { return grey; }
+};
+// è®¡æ—¶
+class MyTimer {
+  private:
+    int _freq;
+    LARGE_INTEGER _begin;
+    LARGE_INTEGER _end;
+
+  public:
+    long costTime; // èŠ±è´¹çš„æ—¶é—´(ç²¾ç¡®åˆ°å¾®ç§’)
+  public:
+    MyTimer() {
+        LARGE_INTEGER tmp;
+        QueryPerformanceFrequency(&tmp);
+        _freq = tmp.QuadPart;
+        costTime = 0;
+    }
+    void Start() { // å¼€å§‹è®¡æ—¶
+        QueryPerformanceCounter(&_begin);
+    }
+    void End() { // ç»“æŸè®¡æ—¶
+        QueryPerformanceCounter(&_end);
+        costTime = (long)((_end.QuadPart - _begin.QuadPart) * 1000 / _freq);
+    }
+    void Reset() { // è®¡æ—¶æ¸…0
+        costTime = 0;
+    }
+};
 
 bool isSameBlock(Mat f, Mat mark, Point lt, Point rb, int xgm);
 void mark(Mat &mark, Point lt, Point rb);
 void EnCode(Mat &R, int height, int width, vector<char> &Q);
 void Decode(Mat &R, int height, int width, const vector<char> &Q);
 Point REHelper(Mat f, Mat mark, int M, int N, Point start, int xgm);
-void RNAMCEncoding(Mat f, Mat &R, Mat &markMatrix, int M, int N, vector<Color> &p, vector<char> &q, int xgm);
-void RDHelper(Mat& R, Color p1);
-void RNAMCDecoding(Mat& R, int M, int N, vector<Color> p, vector<char> q);
+void RNAMCEncoding(Mat f, Mat &R, Mat &markMatrix, int M, int N,
+                   vector<Color> &p, vector<char> &q, int xgm);
+void RDHelper(Mat &R, Color p1);
+void RNAMCDecoding(Mat &R, int M, int N, vector<Color> p, vector<char> q);
 double PSNR(Mat f, Mat R, int M, int N);
 double BPP(vector<Color> p, int M, int N, vector<char> q);
 void segmentDisplay(Mat &display, vector<Color> p);
